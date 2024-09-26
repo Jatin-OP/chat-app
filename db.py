7616076685091b6aa0bbd7e05898932882630329
jatin <jatin@geekyant.com>
2024-09-26 15:24:04 +0530

    chat app

---
+++ db.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+
+from user import User
+
+client = MongoClient('mongodb+srv://devilworld:jatin1112003@cluster0.ratif.mongodb.net/')
+
+chat_db = client.get_database('ChatDB')
+users_collection = chat_db.get_collection('users')
+
+def save_user(name, email, password):
+    hashed_password = generate_password_hash(password)
+    users_collection.insert_one({'_id': name, 'email': email, 'password': hashed_password})
+
+def get_user(username):
+    user_data=users_collection.find_one({'_id':username})
+    return User(user_data['_id'] ,user_data['email'] ,user_data['password'] ) if user_data else None
+
+# save_user('ram' , "ram@gmail.com" ,"12345")
