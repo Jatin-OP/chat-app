bbaad5af2906415f17864ed92f8f4c99d6106376
jatin <jatin@geekyant.com>
2024-09-30 17:19:28 +0530

    esxrdcfhtvgjhbnkm

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)  # Adjust the port as needed
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)  # Adjust the port as needed
